12a1a7273000d1a2bbeaf7daab4798b315325782
TechyPig99 <120288856+TechyPig99@users.noreply.github.com>
2024-03-07 16:19:59 -0600

    Delete html directory

--- html/oregon/php/set_tombstone.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	include ("CommonMethods.php");
-	
-	$sql = "insert into `tombstones` (`name`, `mile`, `epitaph`) values (:name,:mile,:epitaph)";
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute(array(':name' => $_GET['name'], ':mile' => $_GET['mile'], ':epitaph' => $_GET['epitaph']));
-
-?>
